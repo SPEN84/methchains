1c6e2bae4add84d58f465d9e38a1e5819e43dbfa
Anthony Spencer <33986201+SPEN84@users.noreply.github.com>
2019-05-20 10:50:49 -0400

    Add files via upload

---
+++ methodChaining.js
@@ -0,0 +1,18 @@
+const arr = [1,2,3];
+
+const smallerArr = arr.filter(number => number !== 2).map(number => number + 1);
+
+console.log(smallerArr); // => [2,4]
+
+/*-------------------------------------------------*/
+
+
+// Removing Duplicates from an Array
+
+const numbers = [1, 1, 2, 3, 4, 3, 5, 5, 6, 7, 3, 8, 9, 10];
+
+const unique = numbers.filter(function(number, index, array) {
+  return index === array.indexOf(number);
+});
+
+console.log(unique); // => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
